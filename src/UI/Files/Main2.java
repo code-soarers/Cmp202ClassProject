880f3565febc1ed3ccad6dfd50bb68a9f59552c2
Eniola-Anishe <eniola.anishe@binghamuni.edu.ng>
2023-05-19 13:52:33 +0100

    Added the appendData Method

--- src/UI/Files/Main2.java
+++ src/UI/Files/Main2.java
@@ -4,6 +4,7 @@ public class Main2 {
 //
 public static void main(String[] args) {
     FileWriterMain fileWriterMain=new FileWriterMain();
+    fileWriterMain.readFromFile();
    fileWriterMain.appendData("An example of an appended data");
 }
 }
